546528b208a7c086185f9dcc3a690b2cbf8f06de
Patrick Michalina <patrickmichalina@mac.com>
2019-01-11 23:30:30 -0600

    feat: packages include both cjs/esm builds (#4)

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from 'rollup-plugin-commonjs'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    'rxjs/operators',
+    'dgram'
+  ],
+  plugins: [
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json'
+    })
+  ]
+}
+
