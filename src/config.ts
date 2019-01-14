fd8ebd1448af22cfb85f22a01e371fc5bc09b129
Patrick Michalina <patrickmichalina@mac.com>
2019-01-13 21:27:56 -0600

    feat: introduce startProbingONVIFDevices method

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,48 @@
 import { DOMParser } from 'xmldom'
 
+/**
+ * Probe configuration
+ */
 export interface IProbeConfig {
+  /**
+   * Port number.
+   */
   readonly PORT: number
+
+  /**
+   * Multicast address.
+   */
   readonly MULTICAST_ADDRESS: string
+
+  /**
+   * How frequent, in milliseconds, the network is scanned for devices.
+   */
   readonly PROBE_SAMPLE_TIME_MS: number
+
+  /**
+   * Delay, in miliseconds, before the first scan is sent.
+   */
   readonly PROBE_SAMPLE_START_DELAY_TIME_MS: number
+
+  /**
+   * Time it takes to determine if a device is no longer present on the network.
+   * This is best to be at least 1.5x longer than PROBE_SAMPLE_TIME_MS
+   */
   readonly PROBE_NETWORK_TIMEOUT_MS: number
+
+  /**
+   * ONVIF device types to check for.
+   */
   readonly ONVIF_DEVICES: ReadonlyArray<string>
+
+  /**
+   * An object the conforms to the W3C DOMParser spec. This helps parse respnse XML.
+   */
   readonly DOM_PARSER: DOMParser
+  
+  /**
+   * When an attribute is undefined, use this text instead.
+   */
   readonly NOT_FOUND_STRING: string
 }
 
