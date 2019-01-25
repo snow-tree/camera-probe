5d7ecd9d9fcf2f29b35d286d5b875deb66578490
Patrick Michalina <patrickmichalina@mac.com>
2019-01-24 23:48:08 -0600

    reduce payload

--- src/probe-payload.ts
+++ src/probe-payload.ts
@@ -4,20 +4,20 @@ export const probePayload =
   (uuid: string = v4()) =>
     (type: string) =>
 `<?xml version="1.0" encoding="UTF-8"?>
-<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing">
-  <s:Header>
-    <a:Action s:mustUnderstand="1">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</a:Action>
+<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope">
+  <Header xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing">
+    <a:Action mustUnderstand="1">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</a:Action>
     <a:MessageID>uuid:${uuid}</a:MessageID>
     <a:ReplyTo>
       <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>
     </a:ReplyTo>
-    <a:To s:mustUnderstand="1">urn:schemas-xmlsoap-org:ws:2005:04:discovery</a:To>
-  </s:Header>
-  <s:Body >
+    <a:To mustUnderstand="1">urn:schemas-xmlsoap-org:ws:2005:04:discovery</a:To>
+  </Header>
+  <Body>
     <Probe xmlns="http://schemas.xmlsoap.org/ws/2005/04/discovery">
-      <d:Types xmlns:d="http://schemas.xmlsoap.org/ws/2005/04/discovery" xmlns:dp0="http://www.onvif.org/ver10/network/wsdl">
+      <Types xmlns:dp0="http://www.onvif.org/ver10/network/wsdl">
         dp0:${type}
-      </d:Types>
+      </Types>
     </Probe>
-  </s:Body>
-</s:Envelope>`
+  </Body>
+</Envelope>`
