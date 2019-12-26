db0930a6111d50f97b3a87871a8b60883e5d4579
Derek Liu <yjkhtddx@sina.com>
2019-12-26 23:15:24 +0800

    fix: correct ws-probe xml body (#44)

--- src/ws-discovery/ws-probe.ts
+++ src/ws-discovery/ws-probe.ts
@@ -5,7 +5,7 @@ import { probe } from '../core/probe'
 import { IWsProbeConfig, IWsResponse } from './ws-probe.interfaces'
 import { DEFAULT_WS_PROBE_CONFIG } from './config'
 
-const mapDeviceStrToPayload = (str: string) => generateWsDiscoveryProbePayload(str)(generateGuid())
+const mapDeviceStrToPayload = (str: string) => generateWsDiscoveryProbePayload(generateGuid())(str)
 const mapDevicesToPayloads = (devices: readonly string[]) => devices.map(mapDeviceStrToPayload)
 
 export const wsProbe =
